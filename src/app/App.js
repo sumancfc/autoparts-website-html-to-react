072c45d77ab0c6fc00d51296c5b921e3333bf076
sumancfc <sumanstha999@gmail.com>
2020-11-18 16:45:30 +0545

    Scroll to top added

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import ScrollToTop from "react-scroll-to-top";
 import Home from "../pages/Home";
 import Shop from "../pages/Shop";
 
 const App = () => {
   return (
     <>
+      <ScrollToTop smooth color='#fff' />
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/shop' exact component={Shop} />
